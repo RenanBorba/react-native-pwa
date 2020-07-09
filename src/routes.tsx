22dbbddf841c6109dadf4554d8297463e029c6ca
Renan Borba <renandbm.rb@gmail.com>
2020-07-09 17:43:18 -0300

    importacao react-native-pwa

---
+++ src/routes.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { createBottomTabNavigator }
+  from '@react-navigation/bottom-tabs';
+import { NavigationContainer }
+  from '@react-navigation/native';
+
+import { Feather } from '@expo/vector-icons';
+
+import Main from './pages/Main';
+import Camera from './pages/Camera';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          options={{ tabBarIcon: ({ color }) =>
+            <Feather
+              name="home"
+              size={20}
+              color={ color }
+            /> }}
+          name="Main"
+          component={ Main }
+        />
+
+        <Tab.Screen
+          options={{ tabBarIcon: ({ color }) =>
+          <Feather
+            name="camera"
+            size={20}
+            color={ color }
+          /> }}
+          name="Camera"
+          component={ Camera }
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
